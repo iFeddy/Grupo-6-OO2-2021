b8030a58aa32bc1339a3f265a7b791c1931a2dfe
Federico Perez <feddy_7990@hotmail.com>
2021-06-01 12:15:27 -0300

    Agregado Layout Principal de Permisos

--- src/main/java/com/unla/app/controllers/PermisosController.java
+++ src/main/java/com/unla/app/controllers/PermisosController.java
@@ -1,48 +1,26 @@
 package com.unla.app.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.servlet.http.HttpSession;
+
+import com.unla.app.entities.Users;
+import com.unla.app.helpers.ConfigHelper;
+import com.unla.app.helpers.RouteHelper;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@CrossOrigin(origins = "*", maxAge = 3600L)
-@RequestMapping("/admin/")
+@RequestMapping("/")
 public class PermisosController {
-	//@Autowired
-	//private IUserService usuarioService;  
+    
+    @GetMapping({"/permisos", "index", "permisos.index"})
+    public ModelAndView index() {       
+        ModelAndView view = new ModelAndView(RouteHelper.PERMISOS);
+        view.addObject("title", "Inicio - " + ConfigHelper.appName);
+        view.addObject("appName", ConfigHelper.appName);
+        return view;
+    }   
 
-	//@Autowired
-	//private IUserRoleService userRoleService;
-
-	// GET Listado de Permisos
-	/*@GetMapping({ "/permisos", "index", "permisos.index" })
-	public ModelAndView index(@RequestParam(name = "page", defaultValue = "0") int page, Model model, HttpSession session){
-		return mHelper.AuthMiddleware(mHelper.RoleMiddleware(view, 25, roles));
-	}*/
-
-	// POST Guardar Permiso Nuevo
-	//@PostMapping({ "/permisos", "store", "permisos.store" })
-	/*public String store(@Valid Users user, BindingResult result, Model model, RedirectAttributes flash) {
-
-		return "redirect:/admin/permisos";
-	}*/
-
-
-	// POST Editar Permisos / Guardar edición
-	//@PostMapping({ "/permisos/{id}", "update", "permisos.update" })
-	/*public String update(@Valid Users user, @PathVariable(value = "id") Long id, BindingResult result, Model model, RedirectAttributes flash, HttpSession session) {
-		
-		return "redirect:/admin/permisos";
-	}*/
-
-	// DELETE Eliminar Permiso
-	//@DeleteMapping({ "/permisos/{id}", "destroy", "permisos.destroy" })
-	/*public String destroy(@PathVariable(value = "id") Long id, RedirectAttributes flash) {
-	
-		return "redirect:/admin/permisos";
-	}*/
 }
--- src/main/java/com/unla/app/helpers/RouteHelper.java
+++ src/main/java/com/unla/app/helpers/RouteHelper.java
@@ -4,6 +4,7 @@ public class RouteHelper {
 	// www
 	public final static String INDEX = "home/index";
 	public final static String LOGIN = "home/login";
+	public final static String PERMISOS = "home/permisos";
 
 	// errores
 	public final static String ERROR = "errors/error";
@@ -14,10 +15,11 @@ public class RouteHelper {
 	public final static String DASHBOARD = "admin/dashboard";
 	public final static String DASHBOARD_USERS = "admin/users";
 	public final static String DASHBOARD_ROLES = "admin/roles";
-	
+
 	public final static String DASHBOARD_NEW_USERS = "admin/users_add";
-	public final static String DASHBOARD_EDIT_USERS = "admin/users_edit";  
-  
-  public final static String DASHBOARD_NEW_ROLES = "admin/roles_add";
+	public final static String DASHBOARD_EDIT_USERS = "admin/users_edit";
+
+	public final static String DASHBOARD_NEW_ROLES = "admin/roles_add";
 	public final static String DASHBOARD_EDIT_ROLES = "admin/roles_edit";
+
 }
