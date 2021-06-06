1388149662fd145650728c299d6f086efbc7794e
Federico Perez <feddy_7990@hotmail.com>
2021-06-06 15:57:16 -0300

    Agregado NGROK para prueba de Servidor

--- src/main/java/com/unla/app/helpers/ConfigHelper.java
+++ src/main/java/com/unla/app/helpers/ConfigHelper.java
@@ -4,7 +4,7 @@ public class ConfigHelper {
 
     public static String appName = "Sistema Integrador Cuatrimestral - Grupo Nro. 6";
     public static int listResultadosMax = 5;
-    public static String appUrl = "http://127.0.0.1:8080/"; //cambiar al subir al servidor
+    public static String appUrl = "https://3d310c640bfb.ngrok.io/"; //cambiar al subir al servidor
     
     public ConfigHelper() {
     }
