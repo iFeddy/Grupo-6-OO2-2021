cd33e7b9df70d9ea66f6a97654c2a006fec413cf
Federico Perez <feddy_7990@hotmail.com>
2021-06-06 16:33:55 -0300

    Eliminado Configuracion de appURL

--- src/main/java/com/unla/app/helpers/ConfigHelper.java
+++ src/main/java/com/unla/app/helpers/ConfigHelper.java
@@ -4,8 +4,7 @@ public class ConfigHelper {
 
     public static String appName = "Sistema Integrador Cuatrimestral - Grupo Nro. 6";
     public static int listResultadosMax = 5;
-    public static String appUrl = "https://3d310c640bfb.ngrok.io/"; //cambiar al subir al servidor
-    
+       
     public ConfigHelper() {
     }
         
