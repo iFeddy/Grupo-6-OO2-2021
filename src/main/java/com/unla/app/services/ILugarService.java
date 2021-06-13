7c5188abb5f0431f1842045f1c34e75996514702
Federico Perez <feddy_7990@hotmail.com>
2021-06-13 14:01:22 -0300

    Agregado buscar Lugar por ID

--- src/main/java/com/unla/app/services/ILugarService.java
+++ src/main/java/com/unla/app/services/ILugarService.java
@@ -5,9 +5,12 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import com.unla.app.entities.Lugares;
+import com.unla.app.models.LugarModel;
 
 public interface ILugarService {
 	
+	public LugarModel findOne(Long id);
+
 	public List<Lugares> findAll();
 	
 	public Page<Lugares> findAll(Pageable pageable);
